8d607c86879a80110213a07a230847542b7944fb
Aryan Kumar <63790777+ary27x@users.noreply.github.com>
2023-12-29 21:38:16 +0530

    Update pilot.cpp

--- pilot.cpp
+++ pilot.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "headers\lexer.hpp"
+#include "headers\parser.hpp"
 
 int main(int argc , char ** argv)
 {
@@ -27,18 +28,29 @@ int main(int argc , char ** argv)
     }
 
     std::string sourceCode = buffer.str();
-
+    std::cout << "This is the source code  : " << std::endl << std::endl << sourceCode << std::endl;
 
     Lexer lexer(sourceCode);
     std::vector <Token *> tokens = lexer.tokenize();
     int counter = 0;
     std::cout << "[*] TOKENIZED SUCCESSFULLY!" << std::endl;
+    if (tokens.back()->TYPE != TOKEN_EOF)
+    {
+        Token * EOFNode = new Token();
+        EOFNode->TYPE = TOKEN_EOF;
+        tokens.push_back(EOFNode);
+    }
     for(Token * temp : tokens)
     {
         counter++;
         std::cout << counter << ") " << temp->VALUE << " " << typeToString(temp->TYPE) << std::endl;
     }
-    std::cout << std::endl << "this is the end of the program " << std::endl;
+
+    Parser parser(tokens);
+    AST_NODE * ROOT = parser.parse();
+    std::cout << "[*] NO SYNTAX ERROR !" << std::endl;
+    std::cout << "this is the number of statements " << ROOT->SUB_STATEMENTS.size() << std::endl;
+    std::cout << std::endl << "END " << std::endl;
 
     return 0;
 }
