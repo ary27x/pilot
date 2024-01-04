c6d6e27307c1f17f795cb8a7960b8735ec53a3b4
Aryan Kumar <63790777+ary27x@users.noreply.github.com>
2024-01-05 00:07:38 +0530

    Update example.pi.asm

--- example.pi.asm
+++ example.pi.asm
@@ -1,11 +1,11 @@
 section .data
 
 SRef0 :
-db "hello there!"
+db "Welcome to the program!"
 SRef0_L : equ $-SRef0
 
 SRef1 :
-db "this is a string..."
+db "this is the end of the program !"
 SRef1_L : equ $-SRef1
 
 section .text
@@ -13,18 +13,32 @@ section .text
 global _start
 _start:
 
+push rbp
+mov rbp , rsp
+
 mov rax , 1
 mov rdi , 1
 mov rsi , SRef0
 mov rdx , SRef0_L 
 syscall 
 
+sub rsp , 4
+mov dword [rbp - 4] , 25
+sub rsp , 4
+mov dword [rbp - 8] , 943
+mov dword [rbp - 4] , 943
+mov dword [rbp - 8] , 25
 mov rax , 1
 mov rdi , 1
 mov rsi , SRef1
 mov rdx , SRef1_L 
 syscall 
 
+
+add rsp , 8
+mov rsp , rbp
+pop rbp
+
 mov rax , 60
-mov rdi , 7
+mov rdi , 4
 syscall
