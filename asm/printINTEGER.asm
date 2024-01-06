418fe79a5a25f009a6e625dac550c849a5a8d0d5
Aryan Kumar <63790777+ary27x@users.noreply.github.com>
2024-01-06 10:39:19 +0530

    Create printINTEGER.asm

---
+++ asm/printINTEGER.asm
@@ -0,0 +1,52 @@
+%include "asm/printRAX.asm"
+%include "asm/newLine.asm"
+%include "asm/mod.asm"
+%include "asm/div.asm"
+
+section .text
+
+global _printINTEGER
+_printINTEGER: ; THE ARGUMENT SHOULD BE IN THE RAX REGISTER BEFORE MAKING THE CALL 
+
+push rbx
+push rcx
+push rdx
+
+mov rbx , 10 ; we will be using the 10
+; we will use the rdx for the counter , as the rcx register is used for the return values 
+
+mov rdx , 0 ; we are setting up the counter
+
+_primaryLoop:
+cmp rax , 0
+je _loopCallRAX 
+call _mod
+; we get the digit in the rcx register
+; so we push it onto the stack and increment the counter register
+; which in our case is the rdx register
+push rcx
+add rdx , 1
+call _div ; the floor divison of rax // rbx should now be in rcx
+mov rax , rcx ; we update the rax with the new value
+jmp _primaryLoop ; looping back 
+
+
+_loopCallRAX:
+cmp rdx , 0
+je _exit
+pop rax
+call _printRAX
+sub rdx , 1
+jmp _loopCallRAX
+
+
+_exit:
+call _newLine
+pop rdx
+pop rcx
+pop rbx
+
+ret
+
+
+
