0ac254dbeb76777878a7ce20b6b43b15545c7068
Aryan Kumar <63790777+ary27x@users.noreply.github.com>
2024-04-06 14:26:47 +0530

    added support for background colors + function name conflict bug fix

--- asm/colors.asm
+++ asm/colors.asm
@@ -2,36 +2,72 @@
 
 section .data:
 
-reset:
+__reset:
 db 27,91,0,109
-reset_L : equ $-reset
+__reset_L : equ $-__reset
 
-red:
+__black:
+db 27,91,49,59,51,48,109
+__black_L : equ $-__black
+
+__red:
 db 27,91,49,59,51,49,109
-red_L : equ $-red
+__red_L : equ $-__red
 
-green:
+__green:
 db 27,91,49,59,51,50,109
-green_L : equ $-green
+__green_L : equ $-__green
 
-yellow:
+__yellow:
 db 27,91,49,59,51,51,109
-yellow_L : equ $-yellow
+__yellow_L : equ $-__yellow
 
-blue:
+__blue:
 db 27,91,49,59,51,52,109
-blue_L : equ $-blue
+__blue_L : equ $-__blue
 
-purple:
+__purple:
 db 27,91,49,59,51,53,109
-purple_L : equ $-purple
+__purple_L : equ $-__purple
   
-cyan:
+__cyan:
 db 27,91,49,59,51,54,109
-cyan_L : equ $-cyan
+__cyan_L : equ $-__cyan
 
-white:
+__white:
 db 27,91,49,59,51,55,109
-white_L : equ $-white
+__white_L : equ $-__white
 
+; ANSI CODE FOR BACKGROUND COLORS
 
+__background_black:
+db 27,91,49,59,52,48,109
+__background_black_L : equ $-__background_black
+
+__background_red:
+db 27,91,49,59,52,49,109
+__background_red_L : equ $-__background_red
+
+__background_green:
+db 27,91,49,59,52,50,109
+__background_green_L : equ $-__background_green
+
+__background_yellow:
+db 27,91,49,59,52,51,109
+__background_yellow_L : equ $-__background_yellow
+
+__background_blue:
+db 27,91,49,59,52,52,109
+__background_blue_L : equ $-__background_blue
+
+__background_purple:
+db 27,91,49,59,52,53,109
+__background_purple_L : equ $-__background_purple
+
+__background_cyan:
+db 27,91,49,59,52,54,109
+__background_cyan_L : equ $-__background_cyan
+
+__background_white:
+db 27,91,49,59,52,55,109
+__background_white_L : equ $-__background_white
